fff477c3f6c57bdc578e27446024d574e0b436f5
Luis David Moreno <11556208+luisdamora@users.noreply.github.com>
2024-12-04 12:16:51 +0000

    ajustes para ser publico el repo

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+import Sitemap from 'vite-plugin-sitemap'
+
 
 
 export default defineConfig({
-  base: '/json-to-vtt-caprinosol/'
+  base: '/json-to-vtt-caprinosol/',
+  plugins: [
+    Sitemap({
+      hostname: 'https://luisdamora.github.io/json-to-vtt-caprinosol/',
+      dynamicRoutes: [
+        '/',
+      ],
+      outDir: 'dist'
+    }),
+  ],
 })
 
